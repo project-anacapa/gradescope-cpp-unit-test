2404cecf96ba04934ec1e3b742aec8bcbf97e463
Phill Conrad <pconrad@cs.ucsb.edu>
2018-04-04 12:42:43 -0700

    update tddFuncs

--- tddFuncs.cpp
+++ tddFuncs.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdlib>
+#include <cassert>
+#include <vector>
 using std::cout;
 using std::endl;
 using std::cerr;
@@ -13,15 +16,26 @@ using std::ostringstream;
 
 #define FILENAME "results.json"
 
+
 class GradescopeReporter {
 
 private:
+  struct TestCase {
+    double score;
+    double max_score;
+    std::string name;
+    std::string output;
+  };
   ifstream resultsIn;
   ofstream resultsOut;
   Json::Value root;
+  bool inGroup;
+  double groupMax;
+  const char* groupName;
+  std::vector<TestCase> testGroup;
   
 public:
-  GradescopeReporter() {
+  GradescopeReporter() : inGroup(false) {
     resultsIn.open(FILENAME);
     if (resultsIn.fail()) {
       makeEmptyTestsArray();
@@ -31,6 +45,10 @@ public:
   }
   
   ~GradescopeReporter() {
+    if (inGroup) {
+      std::cerr << "WARNING: MISSING END_GROUP for START_GROUP(" << groupMax << "," << groupName <<"); AUTOMATICALLY CALLED" << std::endl;
+      END_GROUP();
+    }
     resultsOut.open(FILENAME);
     resultsOut << root << endl;
     resultsOut.close();    
@@ -43,10 +61,10 @@ public:
     root["tests"] = emptyArray;
   }
   
-  void addTest(int score,
-			    int max_score,
-			    std::string name,
-			    std::string output) {
+  void addTestToJSON(int score,
+          int max_score,
+          std::string name,
+          std::string output) {
     Json::Value newTest;
     newTest["score"] = score;
     newTest["max_score"] = max_score;
@@ -54,58 +72,132 @@ public:
     newTest["output"] = output;
     root["tests"].append(newTest);
   }
+
+  void addTestToGroup(double score,
+          double max_score,
+          std::string name,
+          std::string output) {
+    TestCase tc = {score, max_score, name, output};
+    testGroup.push_back(tc);
+  }
+  
+  void addTest(double score,
+			    double max_score,
+			    std::string name,
+			    std::string output) {
+
+    if (inGroup) {
+      addTestToGroup(score, max_score, name, output);
+    } else {
+      addTestToJSON(score, max_score, name, output);
+    }
+  }
+
+  void startgroup(double points, const char* const name){
+    if (inGroup) {
+      std::cerr << "ERROR: NESTED GROUPS NOT ALLOWED" << std::endl;
+      exit(1);
+    }
+    inGroup = true;
+    groupMax = points;
+    groupName = name;
+    assert(testGroup.empty());
+    
+
+  }
+  void endgroup(){
+    if (!inGroup) {
+      std::cerr << "ERROR: END_GROUP HAS NO MATCHING START_GROUP" << std::endl;
+      exit(1);
+    }
+    inGroup = false;
+    
+    double totalGroupPossible = 0.0;
+    double totalGroupActual = 0.0;  
+    std::string combinedMessage = "";
+
+    for (auto tc: testGroup) {
+      totalGroupActual += tc.score;
+      totalGroupPossible += tc.max_score;
+
+      combinedMessage += tc.output;
+    }
+    if(totalGroupPossible != 0.0){
+      double scaledActual = (totalGroupActual / totalGroupPossible ) * groupMax;
+
+      addTestToJSON(scaledActual, groupMax, groupName, combinedMessage);
+
+      
+    }
+    testGroup.clear();
+  }
   
 };
 
 static GradescopeReporter gsr; // constructor executes before main, destructor after
 
 
-void assertEquals(std::string expected, 
-                  std::string actual, 
+void assertEquals(const char* expected, 
+                  const char* actual, 
                   std::string message) {
-  std::ostringstream ea;
-  ea << "Expected: " << expected << " Actual: " << actual; 
+  /*std::ostringstream ea;
+  //ea << "Expected: " << expected << " Actual: " << actual; 
     
-  if (expected==actual) {
-    cout << "PASSED: " << message << endl;
-    gsr.addTest(1,1,message,ea.str());
+  if (strcmp(expected, actual)==0) {
+    ea << "PASSED: " << message << std::endl;
+    
+    addTest(1,1,message, ea.str());
   } else {
-    cout << "   FAILED: " << message << endl
-         << "     " << ea.str() << endl;
-    gsr.addTest(0,1,message,ea.str());
+    ea << "   FAILED: " << message << std::endl
+         << "     " << "Expected: " << expected << " Actual: " << actual << std::endl;
+    addTest(0,1,message, ea.str());
   }
+  std::cout << ea.str();*/
+  assertEquals(std::string(expected), std::string(actual), message);
 }
 
-void assertEquals(int expected, 
-                  int actual, 
+void assertEquals(const char* expected, 
+                  std::string actual, 
                   std::string message) {
-  std::ostringstream ea;
-  ea << "Expected: " << expected << " Actual: " << actual; 
-    
-  if (expected==actual) {
-    cout << "PASSED: " << message << endl;
-    gsr.addTest(1,1,message,ea.str());
-  } else {
-    cout << "   FAILED: " << message << endl
-         << "     " << ea.str() << endl;
-    gsr.addTest(0,1,message,ea.str());
-  }
+
+  assertEquals(std::string(expected), actual, message);
+
+}
+
+
+
+void addTest(double score,
+          double max_score,
+          std::string name,
+          std::string output){
+
+  gsr.addTest(score, max_score,name, output);
+
 }
 
 void assertTrue(bool expression,
                   std::string message) {
 
   std::ostringstream ea;
-  ea << "Expected expression to be true but it was false "; 
     
   if (expression) {
-    cout << "PASSED: " << message << endl;
+    ea << "| PASSED: " << message << endl;
     gsr.addTest(1,1,message,ea.str());
   } else {
-    cout << "   FAILED: " << message << endl
-         << "     " << ea.str() << endl;
+    ea << "|   FAILED: " << message << endl;
     gsr.addTest(0,1,message,ea.str());
   }
 
+  cout << ea.str();
 
+
+}
+
+void START_GROUP(double points, const char* const name){
+  gsr.startgroup(points, name);
+}
+
+void END_GROUP(){
+  gsr.endgroup();
+  
 }
--- tddFuncs.h
+++ tddFuncs.h
@@ -2,21 +2,53 @@
 #define TDDFUNCS_H
 
 #include <string>
-
-void assertEquals(std::string expected, 
-		  std::string actual, 
-		  std::string message="");
-
-void assertEquals(int expected, 
-		  int actual, 
-		  std::string message="");
-
+#include <sstream>
+#include <iostream>
 
 #define ASSERT_EQUALS(expected,actual) assertEquals(expected,actual,#actual)
 
-
-void assertTrue(bool expression, std::string message="");
-
 #define ASSERT_TRUE(expression) assertTrue(expression,#expression)
 
+void assertEquals(const char* expected, 
+		  const char* actual, 
+		  std::string message);
+
+void assertEquals(const char* expected, 
+		  std::string actual, 
+		  std::string message);
+
+void addTest(double score,
+          double max_score,
+          std::string name,
+          std::string output);
+
+template <class T, class G>
+void assertEquals(T expected, 
+                  T actual, 
+                  G message) {
+  std::ostringstream ea;
+  //ea << "Expected: " << expected << " Actual: " << actual; 
+    
+  if (expected==actual) {
+    ea << "| PASSED: " << message << std::endl;
+    
+    addTest(1,1,message, ea.str());
+  } else {
+    ea << "|   FAILED: " << message 
+       << " Expected: " << expected << " Actual: " << actual << std::endl;
+    addTest(0,1,message, ea.str());
+  }
+  std::cout << ea.str();
+}
+
+
+/*
+void assertEquals(int expected, 
+		  int actual, 
+		  std::string message);
+		  */
+
+void START_GROUP(double points, const char* const name);
+
+void END_GROUP();
 #endif // TDDFUNCS_H
